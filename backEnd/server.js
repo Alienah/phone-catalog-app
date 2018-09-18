c0dbf1b01f810de6962e35516a0d31b75b5405ca
Aida Adalaber Kairosera <aidalbla@gmail.com>
2018-09-18 11:26:07 +0200

    change route from image jason backend

--- backEnd/server.js
+++ backEnd/server.js
@@ -11,7 +11,7 @@ app.get('/api/phones', (request, response) => {
             description: 'Pantalla LCD - 1334 x 750 píxeles - color - 4.7" - 326 ppi - Retina HD display with IPS technology - revestimiento oleofóbico resistente a las huellas dactilares',
             color: 'Oro',
             price: '529',
-            image: './client/src/images/iphone_7_oro.jpg',
+            image: '../frontEnd/src/images/iphone_7_oro.jpg',
             urlImage: 'https://static.fnac-static.com/multimedia/Images/ES/MC/2e/74/55/5600302/1507-1/tsp20170327121118/Telefono-Movil-Apple-Iphone-7-4g-32gb-oro-Smartphone.jpg#0f36ba74-5bcd-4ef4-a422-d6aca062dc24',
             SO: 'IOS 10'
             
@@ -22,7 +22,7 @@ app.get('/api/phones', (request, response) => {
             description: 'Pantalla OLED - 2560 x 1440 píxeles - color - 5.5" - 534 ppi - Quad HD Super AMOLED',
             color: 'Plata',
             price: '369',
-            image: './client/src/images/Samsung_Galaxy_s7_Edge_plata.jpg',
+            image: '../frontEnd/src/images/Samsung_Galaxy_s7_Edge_plata.jpg',
             urlImage: 'https://static.fnac-static.com/multimedia/Images/ES/NR/1c/b0/12/1224732/1505-1.jpg',
             SO: 'Google Android'
         },
@@ -32,7 +32,7 @@ app.get('/api/phones', (request, response) => {
             description: 'Pantalla 5,6" FullHD+ (2.160 x 1.080) 18:9 - Kirin 659 Octa-core Cortex A53 (8 núcleos) a 2,36 GHz',
             color: 'Oro',
             price: '195',
-            image: './client/src/images/Huawei_P_Smart_32gb.jpg',
+            image: '../frontEnd/src/images/Huawei_P_Smart_32gb.jpg',
             urlImage: 'https://static.fnac-static.com/multimedia/Images/ES/NR/27/ec/15/1436711/1505-1.jpg',
             SO: 'Android 8.0 Oreo EMUI 8.0'
         },
@@ -42,7 +42,7 @@ app.get('/api/phones', (request, response) => {
             description: 'Pantalla Retina HD Pantalla panorámica LCD Multi - Touch de 4, 7 pulgadas(en diagonal) con tecnología IPS - Resolución de 1.334 por 750 píxeles a 326 p / p Contraste de 1.400: 1(típico) Brillo máximo 625 cd / m2 True Tone, 3D Touch',
             color: 'Gris espacial',
             price: '689',
-            image: './client/src/images/iPhone_8_64GB_Gris.jpg',
+            image: '../frontEnd/src/images/iPhone_8_64GB_Gris.jpg',
             urlImage: 'https://static.fnac-static.com/multimedia/Images/ES/NR/43/45/15/1393987/1505-1.jpg',
             SO: 'Android 8.0 Oreo EMUI 8.0'
         },
@@ -52,7 +52,7 @@ app.get('/api/phones', (request, response) => {
             description: 'Pantalla OLED - 2960 x 1440 píxeles - color - 5.8" - 570 ppi - Quad HD+ Super AMOLED',
             color: 'Negro',
             price: '455',
-            image: './client/src/images/Samsung_Galaxy_S8.jpg',
+            image: '../frontEnd/src/images/Samsung_Galaxy_S8.jpg',
             urlImage: 'https://static.fnac-static.com/multimedia/Images/ES/NR/18/6d/14/1338648/1505-1.jpg',
             SO: 'Android 7.0 Nougat'
         },
@@ -62,7 +62,7 @@ app.get('/api/phones', (request, response) => {
             description: 'Pantalla LCD - 2280 x 1080 píxeles - color - 5.84" - 432 ppi - 19:9 - LTPS',
             color: 'Azul',
             price: '410',
-            image: './client/src/images/Honor_10.jpg',
+            image: '../frontEnd/src/images/Honor_10.jpg',
             urlImage: 'https://static.fnac-static.com/multimedia/Images/ES/NR/0c/42/19/1655308/1505-1.jpg',
             SO: 'Android 8.1 Oreo con HUAWEI Emotion UI 8.1'
         }
